7f6bd9f5f65157d92e43e9dde05beb2e279eeba0
blinkitten <167661764+blinkitten@users.noreply.github.com>
2024-05-03 16:58:23 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -497,18 +497,7 @@ namespace Blinkit_driver {
 
 
 
-namespace serial {
-    /**
-    * Returns the first byte from the RX buffer or -1 if no data is available.
-    */
-    //% shim=serial::read
-    export function read(): number { return -1 }
-    /**
-    * Returns the number of currently available bytes in the RX buffer.
-    */
-    //% shim=serial::available
-    export function available(): number { return 0 }
-}
+
 
 // while (true) {
 //     while (serial.available() > 0) {
