7fd218b4e51d30ba013906555303da88b08c24b3
blinkitten <167661764+blinkitten@users.noreply.github.com>
2024-05-07 17:20:49 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -85,7 +85,7 @@ namespace Blinkit_driver {
             * @param speed how fast
             */
     //% blockId=Sensor_value_auto
-    //% block="读取blinkit传感器状态"
+    //% block="更新blinkit传感器状态"
     export function normal3(): void {
         //let s: string
         let c = serial.read();
